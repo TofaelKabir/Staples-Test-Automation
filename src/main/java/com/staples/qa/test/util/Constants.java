5d7c34ee61aa79d8b5bcbd95693bef10c1fe9753
tofaelkabir <tofaelkabir@gmail.com>
2020-09-24 22:42:57 -0400

    09242020 10:42pm

---
+++ src/main/java/com/staples/qa/test/util/Constants.java
@@ -0,0 +1,17 @@
+package com.staples.qa.test.util;
+
+public class Constants {
+
+	
+	public static final String HOME_PAGE_TITLE = "Staples® Official Online Store";
+	
+	//will correct later, at present don't need below 4 lines
+	public static final String HOME_PAGE_HEADER = "";
+	public static final String FIND_A_DOCTOR_PAGE_TITLE = "";
+	
+	public static final String DEPARTMENTS_SHEET_NAME = "";
+	public static final String DOCTORSS_SHEET_NAME = "";
+
+}
+
+
